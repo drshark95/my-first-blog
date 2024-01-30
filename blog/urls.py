27d400039c4593652c47d8a8cc5b7832f42b1908
drshark95 <drshark@snu.ac.kr>
2024-01-30 18:44:26 +0900

    Add view and template for detail page

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-] # view 'post_list' assigined at url ''(write nothing)
+    path('', views.post_list, name='post_list'), #  view 'post_list' assigined at url ''(write nothing)
+    path('post/<int:pk>/', views.post_detail, name='post_detail'), # view 'post_detail' assigned at url include post, integer for post_number, '/'
+]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post #.; current dir
 from django.utils import timezone
 
 def post_list(request): # get request
     posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') # instances of all published post with time-order
     return render(request, 'blog/post_list.html', {'posts': posts}) # show post_list html template when parameter='post'
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
