6f77b60da9f0009a85654ea347c70d0eb7497a60
drshark95 <drshark@snu.ac.kr>
2024-01-30 16:57:50 +0900

    Change view to display posts from database

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Post #.; current dir
+from django.utils import timezone
 
 def post_list(request): # get request
-    return render(request, 'blog/post_list.html', {}) # show post_list html template
+    posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') # instances of all published post with time-order
+    return render(request, 'blog/post_list.html', {'posts': posts}) # show post_list html template when parameter='post'
